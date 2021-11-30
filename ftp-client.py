12f4fa26c0907f6c43b198fb277860edfc415ed0
Dima <test>
2021-11-30 17:21:21 +0300

    FTP сервер Богданов Д.А. ПИ20-2

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,13 @@
 import socket
 
 HOST = 'localhost'
-PORT = 6666
+PORT = 6670
 
 while True:
     request = input('>')
-    
     sock = socket.socket()
     sock.connect((HOST, PORT))
-    
     sock.send(request.encode())
-    
     response = sock.recv(1024).decode()
     print(response)
-    
     sock.close()
