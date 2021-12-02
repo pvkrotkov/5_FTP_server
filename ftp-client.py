e65f3c21639440f9c31e6e3e72a70efb294f2879
Anastasia Shadynina <70855182+AnastasiaShad@users.noreply.github.com>
2021-12-02 17:44:38 +0300

    файлы

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,22 @@
 import socket
-
+PORT = 9090
 HOST = 'localhost'
-PORT = 6666
 
-while True:
-    request = input('>')
-    
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
-    response = sock.recv(1024).decode()
-    print(response)
-    
-    sock.close()
+def main():
+    print(f"Присоединились к {HOST} {PORT}")
+    print('help - список команд, exit - выход')
+    while True:
+        req = input('myftp@shell$')
+        if req == 'exit':
+            break
+        with socket.socket() as sock:
+            sock.connect((HOST, PORT))
+
+            sock.send(req.encode())
+            response = sock.recv(1024).decode()
+            if response:
+                print('response:',response)
+
+
+if __name__ == '__main__':
+    main()
