728ccb589b04abe94d7afb49b04395438b3e286f
Гагарин Ярослав <90040588+exerius@users.noreply.github.com>
2021-12-03 16:20:24 +0300

    Update ftp-server.py

--- ftp-server.py
+++ ftp-server.py
@@ -1,17 +1,11 @@
 import socket
 import os
 from shutil import rmtree
-import pickle
-'''
-pwd - показывает название рабочей директории
-ls - показывает содержимое текущей директории
-cat <filename> - отправляет содержимое файла
-'''
 
 dirname = os.path.join(os.getcwd(), 'docs')
 
+
 def process(req):
-    print("".join((list(req)[:3])))
     if req == 'pwd':
         return dirname
     elif req == 'ls':
@@ -21,20 +15,20 @@ def process(req):
         return f'Created dir: {"".join((list(req)[6:]))}'
     elif "".join((list(req)[:5])) == "rmdir":
         try:
-            rmtree(os.path.join(dirname,"".join((list(req)[6:]))))
+            rmtree(os.path.join(dirname, "".join((list(req)[6:]))))
             return f'Папка {"".join((list(req)[6:]))} удалена'
         except NotADirectoryError:
             return "Это не папка!"
     elif "".join((list(req)[:2])) == "rm":
         try:
-            os.remove(os.path.join(dirname,"".join((list(req)[3:]))))
+            os.remove(os.path.join(dirname, "".join((list(req)[3:]))))
             return f'Файл {"".join((list(req)[3:]))} удален'
         except IsADirectoryError:
             return "Это не файл!"
     elif "".join((list(req)[:3])) == "mv ":
         names = "".join(list(req)[3:]).split(" ")
         os.rename(os.path.join(dirname, names[0]), os.path.join(dirname, names[1]))
-        return  "Успешно перемещено"
+        return "Успешно перемещено"
     elif req[:8] == "download":
         file1 = os.path.join(dirname, req[9:])
         with open(file1, "rb") as file:
@@ -42,10 +36,12 @@ def process(req):
             conn.send(content)
         return "Файл отправлен"
     elif req[:6] == "upload":
-        file1 = os.path.join(dirname, req[7:])
+        req = req[7:].split("\n")
+        req[1] = req[1].encode()
+        file1 = os.path.join(dirname, req[0])
         with open(file1, "wb") as file:
-            content = conn.recv(1024)
-            file.write(content)
+            content = req[1]
+            file.write(req[1])
         return "Файл получен"
     else:
         return 'bad request'
@@ -57,14 +53,14 @@ sock = socket.socket()
 sock.bind(('', PORT))
 sock.listen()
 print("Прослушиваем порт", PORT)
-
-while True:
+WIP = True
+while WIP:
     conn, addr = sock.accept()
-    
     request = conn.recv(1024).decode()
-    print(request)
-    
-    response = process(request)
-    conn.send(response.encode())
-
+    if request == "exit":
+        conn.send("Работа завершена".encode())
+        break
+    else:
+        response = process(request)
+        conn.send(response.encode())
 conn.close()
