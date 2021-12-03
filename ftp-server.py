2f9c84a357fd96d7a4bedfdf86b3a359f4ada7c3
Гагарин Ярослав <90040588+exerius@users.noreply.github.com>
2021-12-03 15:28:48 +0300

    Update ftp-server.py

--- ftp-server.py
+++ ftp-server.py
@@ -1,5 +1,7 @@
 import socket
 import os
+from shutil import rmtree
+import pickle
 '''
 pwd - показывает название рабочей директории
 ls - показывает содержимое текущей директории
@@ -9,16 +11,42 @@ cat <filename> - отправляет содержимое файла
 dirname = os.path.join(os.getcwd(), 'docs')
 
 def process(req):
-    print("".join((list(req)[:5])))
+    print("".join((list(req)[:3])))
     if req == 'pwd':
         return dirname
     elif req == 'ls':
         return '; '.join(os.listdir(dirname))
     elif "".join((list(req)[:5])) == "mkdir":
-        os.mkdir("".join((list(req)[6:])))
+        os.mkdir(os.path.join(dirname, "".join((list(req)[6:]))))
         return f'Created dir: {"".join((list(req)[6:]))}'
+    elif "".join((list(req)[:5])) == "rmdir":
+        try:
+            rmtree(os.path.join(dirname,"".join((list(req)[6:]))))
+            return f'Папка {"".join((list(req)[6:]))} удалена'
+        except NotADirectoryError:
+            return "Это не папка!"
     elif "".join((list(req)[:2])) == "rm":
-        
+        try:
+            os.remove(os.path.join(dirname,"".join((list(req)[3:]))))
+            return f'Файл {"".join((list(req)[3:]))} удален'
+        except IsADirectoryError:
+            return "Это не файл!"
+    elif "".join((list(req)[:3])) == "mv ":
+        names = "".join(list(req)[3:]).split(" ")
+        os.rename(os.path.join(dirname, names[0]), os.path.join(dirname, names[1]))
+        return  "Успешно перемещено"
+    elif req[:8] == "download":
+        file1 = os.path.join(dirname, req[9:])
+        with open(file1, "rb") as file:
+            content = file.read(1024)
+            conn.send(content)
+        return "Файл отправлен"
+    elif req[:6] == "upload":
+        file1 = os.path.join(dirname, req[7:])
+        with open(file1, "wb") as file:
+            content = conn.recv(1024)
+            file.write(content)
+        return "Файл получен"
     else:
         return 'bad request'
 
