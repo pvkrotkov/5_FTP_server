8d2a372371fa81c11163921db21a0c39addab864
Гагарин Ярослав <90040588+exerius@users.noreply.github.com>
2021-12-03 16:20:52 +0300

    Update ftp-client.py

--- ftp-client.py
+++ ftp-client.py
@@ -5,11 +5,13 @@ PORT = 6666
 
 while True:
     request = input('>')
-    
+
     sock = socket.socket()
     sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
+    if request[:6] != "upload":
+        sock.send((request).encode())
+    else:
+        sock.send((request+"\n").encode())
     if request[:8] == "download":
         content = sock.recv(1024)
         with open(request[9:], "wb") as file:
@@ -20,5 +22,6 @@ while True:
             sock.send(content)
     response = sock.recv(1024).decode()
     print(response)
-    
-    sock.close()
+    if response == "Работа завершена":
+        break
+sock.close()
