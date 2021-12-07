242e10d6463e1c27e5d8cccbe62eb43a6ccd1575
dvaisluk <70915114+dvaisluk@users.noreply.github.com>
2021-12-07 15:30:42 +0300

    Update ftp-client.py

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,21 @@
 import socket
 
-HOST = 'localhost'
-PORT = 6666
+PORT = 9090
 
-while True:
-    request = input('>')
-    
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
-    response = sock.recv(1024).decode()
-    print(response)
-    
-    sock.close()
+
+def _main():
+    print('Чтобы посмотреть список доступных команд, введите help')
+    while True:
+        request = input('$ ')
+        if request == 'exit':
+            break
+        with socket.socket() as sock:
+            sock.connect(('localhost', PORT))
+            sock.send(request.encode())
+            response = sock.recv(1024).decode()
+            if response:
+                print(response)
+
+
+if __name__ == '__main__':
+    _main()
