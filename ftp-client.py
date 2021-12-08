384ecc2388b9dbf742bf7817dd99ebeea505ae59
Merc1337 <51966929+Merc1337@users.noreply.github.com>
2021-12-08 18:34:03 +0300

    Малимон ПИ20-5
    ftp server is done

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,15 @@
 import socket
-
-HOST = 'localhost'
-PORT = 6666
-
-while True:
-    request = input('>')
-    
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
-    response = sock.recv(1024).decode()
-    print(response)
-    
-    sock.close()
+port = 9090
+def ftp_start():
+    print('Чтобы вызвать справку, введите команду write_help')
+    while True:
+        request = input('~ ')
+        if request == 'exit': #выход по запросу пользователя
+            break
+        with socket.socket() as sock:
+            sock.connect(('localhost', port))
+            sock.send(request.encode())  #отправляем на сервер команду
+            response = sock.recv(1024).decode()
+            if response:
+                print(response)
+print(ftp_start())
