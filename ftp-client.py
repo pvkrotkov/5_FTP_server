ff2ad7c3c1647a0ef37e7548fbd043572c548499
shevlyakovava <90023991+shevlyakovava@users.noreply.github.com>
2021-12-08 19:13:54 +0300

    Update ftp-client.py

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,21 @@
 import socket
 
-HOST = 'localhost'
-PORT = 6666
+ip = 'localhost'
+PORT = 4444
+
+
+print('Для того, чтобы узнать список доступных команд введите help')
+
 
 while True:
-    request = input('>')
-    
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
-    response = sock.recv(1024).decode()
-    print(response)
-    
-    sock.close()
+    request = input('$ ')
+    if request == 'exit':
+        break
+    with socket.socket() as sock:
+        sock.connect((ip, PORT))
+        sock.send(request.encode())
+        response = sock.recv(1024).decode()
+        if response:
+            print(response)
+
+
