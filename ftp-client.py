cdfc7212f503810424e9f40b6674c174e07575dc
Belek <belek_seven@mail.ru>
2021-12-20 05:49:30 +0700

    final

--- ftp-client.py
+++ ftp-client.py
@@ -1,20 +1,73 @@
 import socket
+from datetime import datetime
 
-PORT = 9090
+sock = socket.socket()
+sock.setblocking(1)
+print("**************************")
+print("pwd - текущая директория")
+print("ls - содержимое текущей директории")
+print("cat - содержимое файла")
+print("mkdir <name> - создать новую директорию")
+print("rmdir <name> - удалить пустую директорию")
+print("remove <name> - удалить файл")
+print("rename <old> <new> - изменить имя (почему то не работает :( )")
+print("exit - выход")
+print("**************************")
+
+while True:
+    HOST = input('Введите адрес хоста или localhost : \n')
+    if HOST == 'localhost':
+        HOST = '127.0.0.1'
+        break
+    host_l = HOST.split('.')
+    if (0 < int(host_l[0]) < 255) and (0 < int(host_l[1]) < 255) and (0 < int(host_l[2]) < 255) and (
+            0 < int(host_l[3]) < 255):
+        break
+    else:
+        print('Введен неверный формат адреса.')
+while True:
+    PORT = input('Введите номер порта от 1024 до 49151: \n')
+    if 1023 < int(PORT) < 49152:
+        break
+    else:
+        print('Неверный номер порта.')
+        break
 
 
-def _main():
-    while True:
-        request = input('$ ')
-        if request == 'exit':
-            break
-        with socket.socket() as sock:
-            sock.connect(('localhost', PORT))
-            sock.send(request.encode())
-            response = sock.recv(1024).decode()
-            if response:
-                print(response)
+sock.connect((HOST, int(PORT)))
+while True:
+    data = sock.recv(1024)
+    print(data.decode())
 
+    if data.decode() == "Как Вас зовут?":
+        name = input()
+        sock.send(name.encode())
 
-if __name__ == '__main__':
-    _main()
+    if data.decode() == "Придумайте пароль:":
+        password = input()
+        if password != "":
+            sock.send(password.encode())
+
+    if data.decode() == "Введите пароль: " or data.decode() == "Неверный пароль. Попробуйте еще раз.":
+        password = input()
+        sock.send(password.encode())
+
+    if "Добро пожаловать" in data.decode():
+        break
+
+msg = ""
+while msg != 'exit':
+    msg = input('>')
+    file = open('server.log', 'a')
+    file.write(f"{datetime.now()} соединение установлено \n \n")
+    file.close()
+    if msg == 'exit':
+        file=open('server.log','a')
+        file.write(f"{datetime.now()} закрыто соединение \n \n")
+        print('Клиент закрыт')
+        file.close()
+    sock.send(msg.encode())
+    data = sock.recv(1024)
+    print(data.decode())
+
+sock.close()
