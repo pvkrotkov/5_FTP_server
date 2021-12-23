e1fe13115ac856b4314547e27b9bade8e656c6c8
polina204539 <90207185+polina204539@users.noreply.github.com>
2021-12-23 03:15:45 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,20 +1,19 @@
 import socket
 
- PORT = 9090
+PORT = 9090
 
 
- def _main():
-     while True:
-         request = input('$ ')
-         if request == 'exit':
-             break
-         with socket.socket() as sock:
-             sock.connect(('localhost', PORT))
-             sock.send(request.encode())
-             response = sock.recv(1024).decode()
-             if response:
-                 print(response)
+def _main():
+    while True:
+        request = input('$ ')
+        if request == 'exit':
+            break
+        with socket.socket() as sock:
+            sock.connect(('localhost', PORT))
+            sock.send(request.encode())
+            response = sock.recv(1024).decode()
+            if response:
+                print(response)
 
-
- if __name__ == '__main__':
-     _main()
+if __name__ == '__main__':
+    _main()
