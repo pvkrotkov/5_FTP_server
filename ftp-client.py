42234ddfa512236d394b40606237d50e8f01ea6f
polina204539 <90207185+polina204539@users.noreply.github.com>
2021-12-23 03:17:08 +0300

    Update ftp-client.py

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,1 @@
-import socket
 
-HOST = 'localhost'
-PORT = 6666
-
-while True:
-    request = input('>')
-    
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
-    response = sock.recv(1024).decode()
-    print(response)
-    
-    sock.close()
